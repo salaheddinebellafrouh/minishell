23ec61caddbff342bd9f882970737161ce6b50fd
Nawfal1chaknan <naoufal.chaknan@gmail.com>
2023-06-23 22:47:52 +0100

    new .h

--- minishell.h
+++ minishell.h
@@ -3,24 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sbellafr <sbellafr@student.1337.ma>        +#+  +:+       +#+        */
+/*   By: nchaknan <nchaknan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/15 17:46:32 by sbellafr          #+#    #+#             */
-/*   Updated: 2023/06/17 10:41:40 by sbellafr         ###   ########.fr       */
+/*   Updated: 2023/06/23 22:47:30 by nchaknan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 #define MINISHELL_H
 
+typedef struct s_redirection
+{
+    char **input_file;
+    char **output_file;
+    char **append_file;
+} t_redirection;
 
 typedef struct s_list
 {
-    int var;
+    char    *cmd;
+    char    **arg;
+    int     flag;
+    int     pipe;
+    t_redirection redirect;
     struct s_list *next;
     struct s_list *prev;
-    
 } t_list;
+
+
 char	*ft_substr(char const *s, unsigned int start, int len);
 int     ft_strlen(const char *str);
 #include <stdio.h>
